6133148f513bd1123e9a88a3c9e35a52a4de802b
SofiaPalazuelos <167236863+SofiaPalazuelos@users.noreply.github.com>
2024-12-20 21:43:53 -0800

    Add files via upload

---
+++ src/componentes/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>© 2024 La Fotografía y el Arte Visual. Todos los derechos reservados.</p>
+    </footer>
+  );
+}
+
+export default Footer;
